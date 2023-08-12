1c0a5fe7cf5b1a69715e1a7bf46d24f57233e034
DanielBelmes <3631206+DanielBelmes@users.noreply.github.com>
2023-08-12 10:51:04 -0700

    IR of vulkan api

--- tools/generator/common.nim
+++ tools/generator/common.nim
@@ -1,10 +1,269 @@
 # std dependencies
-import std/xmltree
+import std/xmltree, std/tables, std/sets
 
+type TypeCategory* {.pure.}= enum
+  Bitmask,
+  BaseType,
+  Constant,
+  Define,
+  Enum,
+  ExternalType,
+  FuncPointer,
+  Handle,
+  Include,
+  Struct,
+  Union,
+  Unknown
+
+type TypeData* = object
+  category: TypeCategory = TypeCategory.Unknown
+  requiredBy: OrderedSet[string]
+  xmlLine: int
+
+type TypeInfo* = object
+  prefix*: string
+  `type`*: string
+  postfix*: string
+
+type AliasData* = object
+  name*: string
+  xmlLine*: int
+
+type BaseTypeData* = object
+  typeInfo*: TypeInfo
+  xmlLine*: int
+
+type BitmaskData* = object
+  require*: string
+  `type`*: string
+  xmlLine*: int
+
+type EnumValueData* = object
+  alias*: string
+  bitpos*: string
+  name*: string
+  protect*: string
+  value*: string
+  xmlLine*: int
+
+type EnumData* = object
+  #void addEnumAlias( int line, string const & name, string const & alias, string const & protect, bool supported );
+  #void addEnumValue(int line, string const & valueName, string const & protect, string const & bitpos, string const & value, bool supported );
+
+  bitwidth*: string
+  isBitmask*: bool = false
+  unsupportedValues*: seq[EnumValueData]
+  values*: seq[EnumValueData]
+  xmlLine*: int
+
+type NameData* = object
+  name*: string
+  arraySizes*: seq[string]
+
+type ParamData* = object
+  `type`*: TypeInfo
+  name*: string
+  arraySizes*: seq[string]
+  lenExpression*: string
+  lenParams*: seq[(string, csize_t)]
+  optional*: bool = false
+  strideParam*: (string, csize_t)
+  xmlLine*: int
+
+type CommandData* = object
+  errorCodes*: seq[string]
+  handle*: string
+  params*: seq[ParamData]
+  requiredBy*: OrderedSet[string]
+  returnType*: string
+  successCodes*: seq[string]
+  xmlLine*: int
+
+type ConstantData* = object
+  `type`*: string
+  value*: string
+  xmlLine*: int
+
+type DefineData* = object
+  deprecated*: bool = false
+  require*: string
+  xmlLine*: int
+  deprecationReason*: string
+  possibleCallee*: string
+  params*: seq[string]
+  possibleDefinition*: string
+
+type DefinesPartition* = object
+  callees*: OrderedTable[string, DefineData]
+  callers*: OrderedTable[string, DefineData]
+  values*: OrderedTable[string, DefineData]
+
+type RemoveData* = object
+  commands*: seq[string]
+  enums*: seq[string]
+  types*: seq[string]
+  xmlLine*: int
+
+type RequireData* = object
+  depends*: string
+  commands*: seq[string]
+  constants*: seq[string]
+  types*: seq[string]
+  xmlLine*: int
+
+type ExtensionData* = object
+  deprecatedBy*: string
+  isDeprecated*: bool = false
+  name*: string
+  number*: string
+  obsoletedBy*: string
+  platform*: string
+  promotedTo*: string
+  depends*: OrderedTable[string, seq[seq[string]]]
+  requireData*: seq[RequireData]
+  `type`*: string
+  xmlLine*: int = 0
+
+type FeatureData* = object
+  name*: string
+  number*: string
+  removeData*: seq[RemoveData]
+  requireData*: seq[RequireData]
+  xmlLine*: int
+
+type ExternalTypeData* = object
+  require*: string
+  xmlLine*: int = 0
+
+type ComponentData* = object
+  bits*: string
+  name*: string
+  numericFormat*: string
+  planeIndex*: string
+  xmlLine*: int
+
+type PlaneData* = object
+  compatible*: string
+  heightDivisor*: string
+  widthDivisor*: string
+  xmlLine*: int
+
+type FormatData* = object
+  blockExtent*: string
+  blockSize*: string
+  chroma*: string
+  classAttribute*: string
+  components*: seq[ComponentData]
+  compressed*: string
+  packed*: string
+  planes*: seq[PlaneData]
+  spirvImageFormat*: string
+  texelsPerBlock*: string
+  xmlLine*: int
+
+type FuncPointerArgumentData* = object
+  name*: string
+  `type`*: string
+  xmlLine*: int
+
+type FuncPointerData* = object
+  arguments*: seq[FuncPointerArgumentData]
+  require*: string
+  xmlLine*: int
+
+type HandleData* = object
+  childrenHandles*: OrderedSet[string]
+  commands*: OrderedSet[string]
+  deleteCommand*: string
+  deleteParent*: string
+  deletePool*: string
+  objTypeEnum*: string
+  parent*: string
+  secondLevelCommands*: OrderedSet[string]
+  isDispatchable*: bool
+  xmlLine*: int
+
+  # RAII data
+  #destructorIt*: map<string, CommandData>::const_iterator
+  #constructorIts*: vector<map<string, CommandData>::const_iterator>
+
+type IncludeData* = object
+  xmlLine*: int
+
+type PlatformData* = object
+  protect*: string
+  xmlLine*: int
+
+type MemberData* = object
+  `type`*: TypeInfo
+  name*: string
+  arraySizes*: seq[string]
+  bitCount*: string
+  lenExpressions*: seq[string]
+  lenMembers*: seq[(string, csize_t)]
+  noAutoValidity*: bool
+  optional*: seq[bool]
+  selection*: seq[string]
+  selector*: string
+  value*: string
+  xmlLine*: int
+
+type StructureData* = object
+  allowDuplicate*: bool
+  isUnion*: bool
+  returnedOnly*: bool
+  mutualExclusiveLens*: bool
+  members*: seq[MemberData]
+  structExtends*: seq[string]
+  subStruct*: string
+  xmlLine*: int
+
+type TagData* = object
+  xmlLine*: int
+
+type VectorParamData* = object
+  lenParam*: csize_t = -1
+  strideParam*: csize_t = -1
+
+
+type Registry * = object
+  m_api*: string
+  m_baseTypes*: OrderedTable[string, BaseTypeData]
+  m_bitmaskAliases*: OrderedTable[string, AliasData]
+  m_bitmasks*: OrderedTable[string, BitmaskData]
+  m_commandAliases*: OrderedTable[string, AliasData]
+  m_commands*: OrderedTable[string, CommandData]
+  m_constantAliases*: OrderedTable[string, AliasData]
+  m_constants*: OrderedTable[string, ConstantData]
+  m_defines*: OrderedTable[string, DefineData]
+  m_definesPartition*: DefinesPartition  # partition defined macros into mutually-exclusive sets of callees, callers, and values
+  m_enumAliases*: OrderedTable[string, AliasData]
+  m_enums*: OrderedTable[string, EnumData]
+  m_extendedStructs*:  OrderedSet[string]
+  m_extensions*: seq[ExtensionData]
+  m_externalTypes*: OrderedTable[string, ExternalTypeData]
+  m_features*: seq[FeatureData]
+  m_formats*: OrderedTable[string, FormatData]
+  m_funcPointers*: OrderedTable[string, FuncPointerData]
+  m_handleAliases*: OrderedTable[string, AliasData]
+  m_handles*: OrderedTable[string, HandleData]
+  m_includes*: OrderedTable[string, IncludeData]
+  m_platforms*: OrderedTable[string, PlatformData]
+  m_RAIISpecialFunctions*: OrderedSet[string]
+  m_structAliases*: OrderedTable[string, AliasData]
+  m_structs*: OrderedTable[string, StructureData]
+  m_tags*: OrderedTable[string, TagData]
+  m_types*: OrderedTable[string, TypeData]
+  m_typesafeCheck*: string
+  m_unsupportedExtensions*: OrderedSet[string]
+  m_unsupportedFeatures*: OrderedSet[string]
+  m_version*: string
+  m_vulkanLicenseHeader*: string
 
 type Generator * = object
   doc *:XmlNode
   api *:string ## needs to be of value "vulkan" or "vulkansc"
+  registry*: Registry
 
 const LicensePlate * = """
 # AutoGenerated File
