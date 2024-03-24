33a9114421f1a942d6d5821897ad306db4fda83d
DanielBelmes <3631206+DanielBelmes@users.noreply.github.com>
2024-03-24 16:07:45 -0700

    remove dead code

--- tools/generator.nim
+++ tools/generator.nim
@@ -48,20 +48,5 @@ proc main=
     registry: parser.registry )
   generator.generate()
 
-  # # Parse the XML into our Intermediate Representation objects
-  # generator.readRegistry()
-
-  # # Generate the code files
-  # const C_like = true
-  # generator.generateAPI( C_like )
-  # generator.generateExtensionInspection( C_like )
-  # generator.generateTypes( C_like )
-  # generator.generateFormats( C_like )
-  # generator.generateConsts( C_like )
-  # generator.generateEnums( C_like )
-  # generator.generateProcs( C_like )
-  # generator.generateHandles( C_like )
-  # generator.generateStructs( C_like )
-
 
 when isMainModule: main()
