a82e5674e3de9aba1b32df30830952f792d1b7e7
Daniel Belmes <danielbelmes@ymail.com>
2025-02-16 12:48:36 -0800

    funcpointer fix

--- src/VulkanNim/vulkan_funcpointers.nim
+++ src/VulkanNim/vulkan_funcpointers.nim
@@ -13,4 +13,9 @@ type PFN_vkReallocationFunction* = proc(pUserData: pointer; pOriginal: pointer; 
 type PFN_vkAllocationFunction* = proc(pUserData: pointer; size: csize_t; alignment: csize_t; allocationScope: VkSystemAllocationScope): pointer {.cdecl.}
 type PFN_vkFreeFunction* = proc(pUserData: pointer; pMemory: pointer): void {.cdecl.}
 type PFN_vkVoidFunction* = proc(): void {.cdecl.}
+type PFN_vkDebugReportCallbackEXT* = proc(flags: VkDebugReportFlagsEXT; objectType: VkDebugReportObjectTypeEXT; `object`: uint64; location: csize_t; messageCode: int32; pLayerPrefix: cstring; pMessage: cstring; pUserData: pointer): VkBool32 {.cdecl.}
+type PFN_vkDebugUtilsMessengerCallbackEXT* = proc(messageSeverity: VkDebugUtilsMessageSeverityFlagBitsEXT; messageTypes: VkDebugUtilsMessageTypeFlagsEXT; pCallbackData: VkDebugUtilsMessengerCallbackDataEXT; pUserData: pointer): VkBool32 {.cdecl.}
+type PFN_vkFaultCallbackFunction* = proc(unrecordedFaults: VkBool32; faultCount: uint32; pFaults: VkFaultData): void {.cdecl.}
+type PFN_vkDeviceMemoryReportCallbackEXT* = proc(pCallbackData: VkDeviceMemoryReportCallbackDataEXT; pUserData: pointer): void {.cdecl.}
+type PFN_vkGetInstanceProcAddrLUNARG* = proc(instance: VkInstance; pName: cstring): PFN_vkVoidFunction {.cdecl.}
 
